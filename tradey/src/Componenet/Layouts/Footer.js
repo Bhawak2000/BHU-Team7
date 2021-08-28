f55ea0aeac95bb3fcf41e520a122d7bcd220fdf0
Bhawak2000 <bhawak.anand.civ19@iitbhu.ac.in>
2021-08-28 14:17:47 +0530

    add last touch

--- tradey/src/Componenet/Layouts/Footer.js
+++ tradey/src/Componenet/Layouts/Footer.js
@@ -1,16 +1,15 @@
 import fb from "../Images/fb.svg"
 import insta from "../Images/insta.svg"
-import twitter from "../Images/twitter.svg"
-
+import linkedin from "../Images/linkedin.svg"
 const Footer = () => {
   return (
     <footer className="text-center sticky-bottom">
       <section className="d-flex justify-content-center justify-content-lg-between">
         <div className="mx-auto">
           <h3 style={{ display: "inline-block", padding: "1rem", color: "white" }}>Follow Us: </h3>
-          <img src={fb} className="img-fluid pb-2" alt="fb"></img>
-          <img src={twitter} className="img-fluid pb-2" alt="twitter"></img>
-          <img src={insta} className="img-fluid pb-2" alt="insta"></img>
+          <a href="https://m.facebook.com/100042423789776/" style={{ textDecoration: "none" }}><img src={fb} className="img-fluid pb-2" alt="fb" style={{ cursor: "pointer" }}></img></a>
+          <a href="https://www.linkedin.com/in/bhawak-anand-761649190/" style={{ textDecoration: "none" }}><img src={linkedin} className="img-fluid pb-2 mr-1" alt="linkedin" style={{ cursor: "pointer" }} /> </a>
+          <a href="https://www.instagram.com/nikhithaa_reddy_/" style={{ textDecoration: "none" }}> <img src={insta} className="img-fluid pb-2 mx-3" alt="insta" style={{ cursor: "pointer" }}></img></a>
         </div>
       </section>
     </footer>
